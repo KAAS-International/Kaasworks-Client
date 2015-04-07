5c601cf558543d84a8235677e38cc265024f8ccd
Kevin Voorn <info@kevinvoorn.com>
2015-04-07 16:09:16 +0200

    Comments

--- src/nl/kaasintl/main/GameManager.java
+++ src/nl/kaasintl/main/GameManager.java
@@ -5,19 +5,21 @@ package nl.kaasintl.main;
  */
 public class GameManager
 {
+    // Constructor
     public GameManager() {}
 
+    // TODO: Return if it is your turn
     public boolean isTurn() {
         return true;
     }
 
+    // TODO: Reset game
     public void reset() {}
 
+    // TODO: Check if move is valid
     public boolean isValid(Field f) {
         if (f == f) return true;
         return false;
     }
-
-
 
 }
