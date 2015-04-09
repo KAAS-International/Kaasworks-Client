3ae755f48569d205693e1dd95d6c10066d09d0c0
Kevin Voorn <info@kevinvoorn.com>
2015-04-09 15:15:09 +0200

    Method stubs for validation

--- src/nl/kaasintl/api/RuleManager.java
+++ src/nl/kaasintl/api/RuleManager.java
@@ -4,4 +4,10 @@ package nl.kaasintl.api;
  * Created by David on 4-4-2015.
  */
 public abstract class RuleManager {
+
+    public boolean isValid(int i) { return true; }
+
+    public boolean isOccupied(int i) { return true; }
+
+    public boolean isReal(int i) { return true; }
 }
