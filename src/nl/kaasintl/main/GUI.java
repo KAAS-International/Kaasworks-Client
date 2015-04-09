c20d85788a5a869dbda8fd47cf71cf4e17a31f30
David Visscher <Davidjulianvisscher@hotmail.com>
2015-04-09 16:24:59 +0200

    Player Listing Works in GUI

--- src/nl/kaasintl/main/GUI.java
+++ src/nl/kaasintl/main/GUI.java
@@ -1,6 +1,7 @@
 package nl.kaasintl.main;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 /**
  * Created by david on 8-4-15.
@@ -18,14 +19,23 @@ public class GUI {
     private JTextField   move;
     private JTextArea    gameHistory;
     private JProgressBar timeLeft;
-    private JTable       lobbyTable;
     private JTable       gameBoard;
+    private JList playerList;
+
+    public GUI()
+    {
+        this.gameManager = new GameManager();
+
+        updateLobby();
+    }
 
     public static void main(String[] args)
     {
+        //Game UI
         JFrame frame = new JFrame("GUI");
 
-        try {
+        try
+        {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
@@ -35,11 +45,34 @@ public class GUI {
         } catch (Exception e) {
             // If Nimbus is not available, you can set the GUI to another look and feel.
         }
-        
+
         frame.setContentPane(new GUI().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void updateLobby()
+    {
+        // get list of players
+        ArrayList<String> players = (ArrayList<String>) gameManager.getPlayerList();
+
+        String[] listdata = new String[players.size()];
+
+        for (int i = 0; i < players.size(); i++)
+        {
+            listdata[i] = players.get(i);
+        }
+
+        DefaultListModel<String> newListModel = new DefaultListModel<String>();
+
+        for (String s : listdata)
+        {
+            newListModel.addElement(s);
+        }
+
+        playerList.setModel(newListModel);
+        playerList.updateUI();
     }
 }
