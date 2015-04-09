d200736ac21b16ea57e8b73a4ff321ecf8d61f5a
Kevin Voorn <info@kevinvoorn.com>
2015-04-09 15:43:24 +0200

    Method stubs created for move validation + generic win conditions. Validate methods created for real and occupied moves. #13 #1 #12

--- src/nl/kaasintl/api/Field.java
+++ src/nl/kaasintl/api/Field.java
@@ -6,17 +6,12 @@ import java.lang.Object;
  * Created by Kevin on 4-4-2015.
  */
 public abstract class Field {
-    // Field number on board
-    public int number = 0;
-
-    // Value used in minimax
     public int value = 0;
-
-    public int getNumber() {
-        return number;
-    }
+    public boolean empty = true;
 
     public int getValue() {
         return value;
     }
+
+    public boolean isEmpty() { return empty; }
 }
--- src/nl/kaasintl/api/RuleManager.java
+++ src/nl/kaasintl/api/RuleManager.java
@@ -5,9 +5,33 @@ package nl.kaasintl.api;
  */
 public abstract class RuleManager {
 
+    // Validate if a move is legal according to game rules
     public boolean isValid(int i) { return true; }
 
-    public boolean isOccupied(int i) { return true; }
+    // Validate if a move is within the game board borders
+    // TODO: This method essentially is the same as isEmpty() in field, we should consider removing it
+    public boolean isOccupied(int i) {
+        if (GameBoard.board.get(i).isEmpty()) return false;
 
-    public boolean isReal(int i) { return true; }
+        return true;
+    }
+
+    // Validate if a move conflicts with an already occupied field
+    public boolean isReal(int i) {
+        if (i < 0) return false;
+        if (i > GameBoard.board.size()-1) return false;
+
+        return true;
+    }
+
+    // Validate if side has met win conditions
+    // TODO: Replace player with actual object
+    public boolean isWin(String player) {
+        return true;
+    }
+
+    // TODO: See above, same issue here
+    public boolean isDraw(String player) {
+        return true;
+    }
 }
