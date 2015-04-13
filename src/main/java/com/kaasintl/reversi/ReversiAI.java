56038ebd6482268462c31d83537e254d5280382b
Kevin Voorn <info@kevinvoorn.com>
2015-04-13 12:04:02 +0200

    Reversi AI changes

--- src/main/java/com/kaasintl/reversi/ReversiAI.java
+++ src/main/java/com/kaasintl/reversi/ReversiAI.java
@@ -3,6 +3,7 @@ package main.java.com.kaasintl.reversi;
 import main.java.com.kaasintl.api.AI;
 import main.java.com.kaasintl.api.Field;
 import main.java.com.kaasintl.api.GameBoard;
+import main.java.com.kaasintl.api.RuleManager;
 
 /**
  * Created by Kevin on 9-4-2015.
@@ -13,13 +14,25 @@ public class ReversiAI extends AI
 
     @Override
     public int nextMove() {
-        return 0;
+        for (Field field : GameBoard.board) {
+            if (0 == 0) { // TODO: This should call RuleManager to verify if field is valid
+                minimax(field.getCoordinate());
+            }
+        }
+
+        return 0; // Return best possible move
     }
 
     // TODO: This method only includes fields with a positive value, needs to be extended with backtracking to include possible bad moves
-    public int[] minimax() {
+    public int[] minimax(int i) {
         for (Field field : GameBoard.board) {
             if (field.getValue() > 0) {
+                moves[moves.length] = field.getCoordinate();
+            }
+        }
+
+        if (moves.length == 0) {
+            for (Field field : GameBoard.board) {
                 moves[moves.length] = field.getCoordinate();
             }
         }
@@ -31,7 +44,11 @@ public class ReversiAI extends AI
     public int[] backtrack() {
 
         for (int i = 0; i < 10; i++) {
-            // Do something (include method minimax() here?)
+
+            for (int move : moves) {
+                minimax(move);
+            }
+
         }
 
         return moves;
