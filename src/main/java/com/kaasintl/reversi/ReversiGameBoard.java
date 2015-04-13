951d0adee118b485e68a3330e22fc4c13c2d0457
Kevin Voorn <info@kevinvoorn.com>
2015-04-13 19:42:33 +0200

    Use ReversiField and ReversiGameBoard instead of generic abstract Field and GameBoard

--- src/main/java/com/kaasintl/reversi/ReversiGameBoard.java
+++ src/main/java/com/kaasintl/reversi/ReversiGameBoard.java
@@ -1,9 +1,17 @@
 package main.java.com.kaasintl.reversi;
 
+import main.java.com.kaasintl.reversi.ReversiField;
 import main.java.com.kaasintl.api.GameBoard;
+
+import java.util.ArrayList;
 
 /**
  * Created by Kevin on 9-4-2015.
  */
 public class ReversiGameBoard extends GameBoard {
+    public static ArrayList<ReversiField> board = new ArrayList<ReversiField>();
+
+    public ArrayList<ReversiField> getBoard() {
+        return board;
+    }
 }
