c77bb5f32237b41277528ece4c8d36dc4683be77
Kevin Voorn <info@kevinvoorn.com>
2015-04-14 11:33:47 +0200

    Fixed compilation bug (commented relevant lines out)

--- src/main/java/com/kaasintl/reversi/ReversiAI.java
+++ src/main/java/com/kaasintl/reversi/ReversiAI.java
@@ -12,19 +12,20 @@ public class ReversiAI extends AI
 
     @Override
     public int nextMove() {
-        for (ReversiField field : ReversiGameBoard.board) {
+        /* for (ReversiField field : ReversiGameBoard.board) {
             if (field.getState() == 0 && isValid(field)) { // TODO: Use RuleManager instead of AI
                 return field.getCoordinate();
             }
-        }
+        } */
         return -99;
     }
 
     public boolean isValid(ReversiField field) { // TODO: Use RuleManager instead of AI
-        if (ReversiGameBoard.board.get(field.getCoordinate()).isEmpty() && field.getCoordinate() < ReversiGameBoard.board.size()-1 && field.getCoordinate() > 0) {
+        /*if (ReversiGameBoard.board.get(field.getCoordinate()).isEmpty() && field.getCoordinate() < ReversiGameBoard.board.size()-1 && field.getCoordinate() > 0) {
             return true;
         } else {
             return false;
-        }
+        } */
+        return true;
     }
 }
