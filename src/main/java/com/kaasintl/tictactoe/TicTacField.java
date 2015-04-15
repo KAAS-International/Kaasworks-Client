e8fb5531f8e54da4de00a124a5b9ff4132837188
Niek Gigengack <info@niekgigengack.nl>
2015-04-15 15:46:05 +0200

    Added method stubs for implementation TicTacToe

--- src/main/java/com/kaasintl/tictactoe/TicTacField.java
+++ src/main/java/com/kaasintl/tictactoe/TicTacField.java
@@ -1,7 +1,14 @@
 package main.java.com.kaasintl.tictactoe;
 
+import main.java.com.kaasintl.api.AI;
+
 /**
  * Created by Niek on 15-4-2015.
  */
-public class TicTacField {
+public class TicTacField extends AI{
+    public int best;
+
+    public int nextMove() {
+        return best;
+    }
 }
