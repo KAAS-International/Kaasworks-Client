e7e1cda961569e742f555eceeb88978dcba48b58
Kevin Voorn <info@kevinvoorn.com>
2015-04-15 17:04:15 +0200

    Cleared ReversiAI to work with new changes and nextMove() now returns Field instead of Force (or int, for boring people)

--- src/main/java/com/kaasintl/reversi/ReversiAI.java
+++ src/main/java/com/kaasintl/reversi/ReversiAI.java
@@ -11,21 +11,7 @@ public class ReversiAI extends AI
     public int best;
 
     @Override
-    public int nextMove() {
-        /* for (ReversiField field : ReversiGameBoard.board) {
-            if (field.getState() == 0 && isValid(field)) { // TODO: Use RuleManager instead of AI
-                return field.getCoordinate();
-            }
-        } */
-        return -99;
-    }
-
-    public boolean isValid(ReversiField field) { // TODO: Use RuleManager instead of AI
-        /*if (ReversiGameBoard.board.get(field.getCoordinate()).isEmpty() && field.getCoordinate() < ReversiGameBoard.board.size()-1 && field.getCoordinate() > 0) {
-            return true;
-        } else {
-            return false;
-        } */
-        return true;
+    public Field nextMove() {
+        return null;
     }
 }
